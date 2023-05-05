48d505ad80c835870ab4b3c696a69cadfb843eee
vitoriaess <vitoriaesswaizer@educar.rs.gov.br>
2023-05-05 17:04:56 -0300

    terceira parte - api

---
+++ bin/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config(); 
+const app = require("..//src/api");
+
+app.use((req, res, next)=> {
+    next();
+});
+console.log(process.env.API_PORT);
+let port = process.env.API_PORT||3001
+app.listen(port);
+
+console.log(`listening on ${port}`);
---
+++ src/api.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+const router = express.Router();
+app.use('/', router.get('/',(req, res)=>{
+    res.status(200).send("<h1>API - CHAT<\h1>")
+}));
+
+app.use("/",router.get("/sobre", (req, res, next) =>{
+    res.status(200).send({
+        "nome": "API-CHAT",
+        "versão": "0.1.0",
+        "autor": "Vitória Swaizer"
+    })
+}));
+
+module.exports=app;
