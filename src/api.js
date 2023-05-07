7af093bb11f9d1659dfc7cf8aba8b7df87dc7855
vitoriaess <vitoriaesswaizer@educar.rs.gov.br>
2023-05-07 16:11:14 -0300

    Base da API

--- src/api.js
+++ src/api.js
@@ -4,11 +4,11 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 const router = express.Router();
-app.use('/', router.get('/',(req, res)=>{
+app.use('/', router.get('/', (req, res)=>{
     res.status(200).send("<h1>API - CHAT<\h1>")
-}));
+}))
 
-app.use("/",router.get("/sobre", (req, res, next) =>{
+app.use("/", router.get("/sobre", (req, res, next) =>{
     res.status(200).send({
         "nome": "API-CHAT",
         "versão": "0.1.0",
